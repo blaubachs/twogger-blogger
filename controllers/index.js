42f58040dd89d3feddd578d41df08eced22d538f
Ben L <veloswim@gmail.com>
2023-02-08 17:55:06 -0800

    added required packages and file structure, boilerplate code

---
+++ controllers/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+
+const frontEndRoutes = require("./frontEndRoutes");
+router.use(frontEndRoutes);
+
+// const apiRoutes = require("./api");
+// router.use("/api", apiRoutes);
+
+module.exports = router;
