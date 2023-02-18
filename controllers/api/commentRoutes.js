b2b442eb4d90d84c70a183b80af1b38844482456
Ben L <veloswim@gmail.com>
2023-02-18 12:45:15 -0800

    added model for comments

---
+++ controllers/api/commentRoutes.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const router = express.Router();
+
+// routes for creating a comment or getting a comment to be created here.
+
+module.exports = router;
--- models/index.js
+++ models/index.js
@@ -1,10 +1,15 @@
 const User = require("./User");
 const Post = require("./Post");
+const Comment = require("./Comments");
 
 Post.belongsTo(User, {
   onDelete: "Cascade",
 });
 User.hasMany(Post);
+
+Comment.belongsTo(Post);
+
+Post.hasMany(Comment);
 
 module.exports = {
   User,
