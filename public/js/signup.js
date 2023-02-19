fd037abcf09c46a2f2fddb43e006179ae8a57479
Ben L <veloswim@gmail.com>
2023-02-19 09:42:22 -0800

    updates

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,1 +1,25 @@
 console.log("signup link");
+const signUpBtn = document.querySelector("#signup");
+
+signUpBtn.addEventListener("click", async (e) => {
+  e.preventDefault();
+  console.log("you submitted the form");
+  const newUserObj = {
+    username: document.querySelector("#signUpUsername").value,
+    password: document.querySelector("#signUpPassword").value,
+  };
+
+  const createNewUser = await fetch("/api/users", {
+    method: "POST",
+    body: JSON.stringify(newUserObj),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  console.log(createNewUser);
+  if (createNewUser.ok) {
+    console.log(createNewUser);
+  } else {
+    console.log("something went wrong");
+  }
+});
