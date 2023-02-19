74f2e8a52f332236b4b61cb2d363353ad600dbbc
Ben L <veloswim@gmail.com>
2023-02-19 10:30:22 -0800

    added logout to home js, added button for new post partial

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -6,5 +6,7 @@ router.get("/", async (req, res) => {
   const postData = await Post.findAll({ include: [User, Comment] });
   res.json(postData);
 });
+
 router.post("/", async (req, res) => {});
+
 module.exports = router;
